b145b8ae70b1fca923ce5581fc2c66ab7a145420
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2019-05-31 23:29:31 +0900

    README with TF setup

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# rovi_utils
+# RoVI Utilities
+
+## Contents
+
+- tf_config/...  
+カメラの構成をTFに設定する
+- r-calib/...  
+カメラとロボットのキャリブレーション
+- floats3pc  
+Numpyデータを任意の座標系のPointCloudに変換
+
+## Build準備  
+- json11  
+~~~
+git clone https://github.com/dropbox/json11
+~~~
+
+## Build
+標準手順どおり(catkin_make)
+
+## カメラの設置手順  
+### tf_config  
+カメラがどこに設置されているか、また設置されている座標(ロボットキャリブレーション結果)をTFへ送るユーティリティがtf_configです。  
+### 起動
+~~~
+roslaunch rovi_utils tf_config.launch
+~~~
+以下のパネルより装置の構成を設定します。
+<img src="img/tf_config.png" />
+
+|ラベル|説明|パラメータ名|
+|:----|:----|:----|
+|フランジフレーム名|ロボットのメカニカルインタフェースのフレーム名|/tf_config/alias/flange|  
+|カメラマウントフレーム名|カメラが取り付けられているフレーム名。固定カメラであれば"world"となります|/tf_config/alias/mount|
+|カメラマウント/カメラ変換|いわゆるロボットキャリブレーションの値|/tf_config/alias/mount|
+
+「保存」を押すとtf_config.yamlファイルに書き込まれます。内容を変更した場合は再起動が必要です。  
+この設定にてtf_configを起動したときのTF_TREEは下図のようになります。
+<img src="img/frames.png" width="400" /><br clear="all" />
+次のロボットキャリブレーションを含め、**ロボットとRoVIを連携する場合は、必ず先にtf_configを起動しておきます**
+
+### r-calib  
+いわゆるロボットキャリブレーションです。これは先のTF_TREEのmount-camera間の座標変換を求めることです。
+
--- src/floats3pc.cpp
+++ src/floats3pc.cpp
@@ -9,14 +9,17 @@
 ros::NodeHandle *nh;
 ros::Publisher *pub;
 
-std::string fl2pc_frameid("/hand");
+static std::map<std::string,std::string> Param{
+  { "frame_id", "world" },
+  { "input_frame_id", "camera" }
+};
 
 void subn(const rovi::Floats& buf){
   int N=buf.data.size()/3;
   ROS_INFO("points=%d",N);
   sensor_msgs::PointCloud pts;
-  pts.header.stamp = ros::Time::now();
-  pts.header.frame_id = fl2pc_frameid;
+  pts.header.stamp=ros::Time::now();
+  pts.header.frame_id=Param["output"];
   pts.points.resize(N);
   for (int n=0,i=0; n<N; n++){
     pts.points[n].x=buf.data[i++];
@@ -26,23 +29,32 @@ void subn(const rovi::Floats& buf){
   pub->publish(pts);
 }
 
+void param(const std_msgs::String& buf){
+  std::string s=buf.data;
+  std::replace(s.begin(),s.end(),'\'','\"');
+  std::cerr<<"float2pc::param "<<s<<std::endl;
+  std::string err;
+  auto dict=json11::Json::parse(s, err);
+  if(dict.object_items().count("frame_id")>0){
+    Param["frame_id"]=dict["frame_id"].string_value();
+  }
+  std::cerr<<"float2pc::frame_id "<<Param["frame_id"]<<std::endl;
+  std::cerr<<"float2pc::input_frame_id "<<Param["input_frame_id"]<<std::endl;
+}
+
 int main(int argc, char **argv){
-  if (argc >= 4){
-    fl2pc_frameid = argv[1];
+  std::cerr<<"argc "<<argc<<std::endl;
+  if (argc >= 6){
+    Param["frame_id"]=argv[1];
+    Param["input_frame_id"]=argv[2];
   }
   ros::init(argc, argv, "floats3pc");
   ros::NodeHandle n;
-  nh = &n;
+  nh=&n;
   ros::Subscriber s0=n.subscribe("floats",1,subn);
+  ros::Subscriber s1=n.subscribe("param",1,param);
   ros::Publisher p0=n.advertise<sensor_msgs::PointCloud>("pc",1);
-  pub = &p0;
-
-  std::string buf="{\"output\":\"hand\",\"input\":\"camera\"}";
-  std::string err;
-  json11::Json v = json11::Json::parse(buf, err);
-  for (auto &k : v.array_items()){
-    std::cout << k.string_value() << std::endl;
-  }
+  pub=&p0;
 
   ros::spin();
   return 0;
