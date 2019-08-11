9d33874fdda2f8f68ba0f37a855d8577a59bbee5
KazukiHiraizumi <ca-giken@c-able.ne.jp>
2019-08-11 09:02:25 +0900

    dashboard is added "confirm" for stopping launch

--- script/dashboard.py
+++ script/dashboard.py
@@ -5,21 +5,26 @@ import roslib
 import rospy
 import sys
 import os
+import time
 import commands
 import subprocess
 import functools
 import re
+from func_timeout import func_timeout, FunctionTimedOut
+
 from std_msgs.msg import Bool
 from std_msgs.msg import String
 
 import Tkinter as tk
 import ttk
+import tkMessageBox
 import tkFileDialog as filedialog
 from tkfilebrowser import askopendirname, askopenfilenames, asksaveasfilename
 
 Config={
   "path":"~/",
-  "geometry":"900x80-0-30"
+  "geometry":"900x80-0-30",
+  "confirm":"Stop anyway"
 }
 Param={
   "recipe":""
@@ -53,9 +58,19 @@ def cb_save():
   if ret != "":
     print "save",ret
 
+stime=0
+callback=None
+def set_timeout(cb,delay):
+  global stime,callback
+  stime=time.time()+delay
+  callback=cb
 ####launch manager############
+msgBox=None
 def cb_run(n):
-  global Items
+  global Items,msgBox
+  if msgBox is not None:
+    msgBox.destroy()
+    msgBox=None
   item=Items[n]
   if "process" not in item:
     proc=subprocess.Popen(["roslaunch",item["package"],item["file"]])
@@ -64,11 +79,38 @@ def cb_run(n):
     item["button"]["text"]="Stop"
     item["process"]=proc
   else:
+    if item["confirm"]:
+      w=item["label"]
+      msgBox=tk.Tk()
+      msgBox.title("Confirm")
+      msgBox.geometry("50x50+"+str(w.winfo_rootx())+"+"+str(w.winfo_rooty()-150))
+      try:
+        f=tkMessageBox.askyesno("Confirm",Config["confirm"],parent=msgBox)
+      except:
+        print "Message box exception"
+        f=False
+      if msgBox is None: return
+      msgBox.destroy()
+      msgBox=None
+      if f is False: return
     item["process"].terminate()
-    item["label"]["background"]="#FF0000"
+    item["label"]["background"]="#FF4500"
     item["label"]["foreground"]="#444444"
     item["button"]["text"]="Start"
     item.pop("process")
+
+def cb_stop(n):
+  global Items
+  item=Items[n]
+  if item["confirm"]:
+    f=tkMessageBox.askyesno("Confirm",Config["confirm"])
+    root.overrideredirect(True)
+    if f is False: return
+  item["process"].terminate()
+  item["label"]["background"]="#FF4500"
+  item["label"]["foreground"]="#444444"
+  item["button"]["text"]="Start"
+  item.pop("process")
 
 ####Message box
 buffer=[]
@@ -126,16 +168,16 @@ frame1.pack(fill='x',anchor='nw')
 frame2=tk.Frame(root,bd=2,background="#FFFFFF")
 frame2.pack(fill='x',anchor='nw',expand=1)
 for key in Config.keys():
-  if key.startswith('launch') is not True:continue
-  item=Config[key]
-  n=len(Items)
-  wlabel=ttk.Label(frame1,text=item["note"],background='#FF0000',foreground='#444444')
-  wlabel.pack(side='left',fill='y',anchor='nw')
-  wbtn=ttk.Button(frame1,text='Start', width=4, command=functools.partial(cb_run,n))
-  wbtn.pack(side='left',fill='y',anchor='nw')
-  ttk.Label(frame1,text=' ',background="#FFFFFF").pack(side='left')
-  item["label"]=wlabel
-  item["button"]=wbtn
+  if key.startswith('launch'):
+    item=Config[key]
+    n=len(Items)
+    wlabel=ttk.Label(frame1,text=item["note"],background='#FF4500',foreground='#444444')
+    wlabel.pack(side='left',fill='y',anchor='nw')
+    wbtn=ttk.Button(frame1,text='Start', width=4, command=functools.partial(cb_run,n))
+    wbtn.pack(side='left',fill='y',anchor='nw')
+    ttk.Label(frame1,text=' ',background="#FFFFFF").pack(side='left')
+    item["label"]=wlabel
+    item["button"]=wbtn
   Items.append(item)
 
 wRecipe=ttk.Label(frame2,text="Recipe ["+Param["recipe"]+"]",background="#0044CC",foreground="#FFFF00")
@@ -143,8 +185,8 @@ wRecipe.grid(row=0,column=1,padx=0,pady=1,sticky='ew',columnspan=2)
 ttk.Button(frame2,text="Open Recipe",command=cb_open_dir).grid(row=1,column=1,padx=1,pady=1,sticky='nsew')
 ttk.Button(frame2,text="Save as",command=cb_save).grid(row=1,column=2,padx=1,pady=1,sticky='nsew')
 text=tk.Text(frame2,width=80,height=3,background="#FFFFCC")
-text.grid(row=0,column=3,padx=0,pady=1,sticky='nsew',rowspan=2)
-text.insert(tk.INSERT,"<<<<<<<<<<<<<<<<<<<messages>>>>>>>>>>>>>>>>>>>")
+text.grid(row=0,column=3,padx=0,pady=1,sticky='nse',rowspan=2)
+text.insert(tk.INSERT,"-"*80)
 text.insert(tk.END,"\n")
 
 while not rospy.is_shutdown():
@@ -152,4 +194,8 @@ while not rospy.is_shutdown():
     while len(buffer)>0:
       s=buffer.pop(0)
       text.insert("1.0",s+"\n")
+  if stime>0:
+    if time.time()>stime:
+      callback()
+      stime=0
   root.update()
