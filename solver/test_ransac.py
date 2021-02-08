b1fb2f89ebffe71caaff8fd0e1bec9aca278c624
t-akita <69621509+t-akita@users.noreply.github.com>
2021-02-08 19:39:49 +0900

    solver/ransac_solver.pyをpython3とopen3d 0.12.0の構文に書き換えた。

--- solver/test_ransac.py
+++ solver/test_ransac.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 import numpy as np
 import open3d as o3d
@@ -14,15 +14,15 @@ Param={
   "rotate":0,
   "repeat":1}
 
-model=o3d.read_point_cloud("../data/model.ply")
-scene=o3d.read_point_cloud("../data/sample.ply")
+model=o3d.io.read_point_cloud("../data/model.ply")
+scene=o3d.io.read_point_cloud("../data/sample.ply")
 model.paint_uniform_color([1, 0.706, 0])
 scene.paint_uniform_color([0, 0.651, 0.929])
-o3d.draw_geometries([model, scene])
+o3d.visualization.draw_geometries([model, scene])
 
 solver.learn([solver.toNumpy(model)],Param)
 result=solver.solve([solver.toNumpy(scene)],Param)
-print "Score",result["fitness"]
-print "Tmat",result["transform"]
+print("Score",result["fitness"])
+print("Tmat",result["transform"])
 model.transform(result["transform"][0])
-o3d.draw_geometries([model, scene])
+o3d.visualization.draw_geometries([model, scene])
